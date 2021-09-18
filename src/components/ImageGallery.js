b325adbceeed9d17998584f5eb58d5c1197cbeb9
breannar888 <67029835+breannar888@users.noreply.github.com>
2021-09-18 17:06:06 -0500

    updated and mapped in gallery images

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -4,15 +4,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 //overrides base ccs file in order to manipulate height
 import "../css/Gallery.css";
-
-//importing pictures from images folder
-import pic1 from "../images/dish1.jpg";
-import pic3 from "../images/dish3.jpg";
-import pic4 from "../images/dish4.jpg";
-import pic5 from "../images/dish6.jpg";
-import pic6 from "../images/dish7.jpg";
-import pic7 from "../images/hero1.jpg";
-import pic8 from "../images/hero3.jpg";
+import IMAGE_DATA from "../textdata/gallerydata";
 
 function ImageGallery() {
   return (
@@ -25,30 +17,17 @@ function ImageGallery() {
       showStatus={false}
       showThumbs={true}
       width={"100%"}
-      dynamicHeight={false}
+      dynamicHeight={true}
       interval={"5000"}
     >
-      <div>
-        <img src={pic1} alt=""/>
-      </div>
-      <div>
-        <img src={pic3} alt=""/>
-      </div>
-      <div>
-        <img src={pic4} alt=""/>
-      </div>
-      <div>
-        <img src={pic5} alt=""/>
-      </div>
-      <div>
-        <img src={pic6} alt=""/>
-      </div>
-      <div>
-        <img src={pic7} alt=""/>
-      </div>
-      <div>
-        <img src={pic8} alt=""/>
-      </div>
+
+    {IMAGE_DATA.map((image) => {
+      return (
+        <div key={image.id}>
+          <img src={image.image} alt=""/>
+        </div>
+      )
+    })}
     </Carousel>
     </div>
   );
---
+++ src/textdata/gallerydata.js
@@ -0,0 +1,52 @@
+const IMAGE_DATA = [
+    {
+        id: '1',
+        image: 'https://img.grouponcdn.com/deal/pRPzhXA5Yx4QgXLTU3MC/ze-2048x1242/v1/c870x524.jpg',
+    },
+    {
+        id: '2',
+        image: 'https://jennifercooks.com/wp-content/uploads/2011/03/creamy-crawfish-etouffee-recipe-2.jpg',
+    },
+    {
+        id: '3',
+        image: 'https://acadianatable.com/wp-content/uploads/2013/03/CrawfishEtouffeePlate.jpg',
+    },
+    {
+        id: '4',
+        image: 'https://img.delicious.com.au/qh3ckgQJ/del/2017/10/crab-omelette-53318-2.jpg',
+    },
+    {
+        id: '5',
+        image: 'https://preventionrd.com/wp-content/uploads/2016/10/Blackened-Fish-Tacos-with-Cilantro-Slaw-and-Sriracha-Mayo-3.jpg',
+    },
+    {
+        id: '6',
+        image: 'https://www.emerils.com/sites/default/files/boudin-balls.jpg',
+    },
+    {
+        id: '7',
+        image: 'https://cdn.shopify.com/s/files/1/0305/5921/articles/Herbed-Butter-Dungy-hero_1800x.jpg?v=1588276553',
+    },
+    {
+        id: '8',
+        image: 'https://www.louisianacookin.com/wp-content/uploads/2020/10/lc_image003_RGB_1080x1080-1.jpg',
+    },
+    {
+        id: '9',
+        image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20191219-seo-steak-eggs-delish-ehg-8987-jpg-1579900349.jpg',
+    },
+    {
+        id: '10',
+        image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/grilled-lobster-tail-004-1553897798.jpg',
+    },
+    {
+        id: '11',
+        image: 'https://petitworldcitizen.files.wordpress.com/2013/12/mousse-cake-2.jpg',
+    },
+    {
+        id: '12',
+        image: 'https://d1uz88p17r663j.cloudfront.net/original/71e3882bba7d1e2783dbe8ecec7e368d_apple_crepes.jpg',
+    },
+]
+
+export default IMAGE_DATA;
