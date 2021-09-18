a9e9e3c45505a73445141f3ec18038f10db3096a
breannar888 <67029835+breannar888@users.noreply.github.com>
2021-09-18 17:17:13 -0500

    fill-screen on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import AboutCard from "../components/AboutCard.js";
 import ContactBar from "../components/ContactBar.js";
 import HeroSection from "../components/HeroSection.js";
-
+import "../css/layout.css";
 
 function HomePage() {
   return (
-    <div>
+    <div className="fill-screen">
       <HeroSection/>
       <AboutCard />
       <ContactBar />
