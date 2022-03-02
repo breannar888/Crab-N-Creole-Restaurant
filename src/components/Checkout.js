e3d46e1bd9b3f83d1db84582237ec576bf9541f5
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-03-01 18:04:17 -0600

    added empty cart display

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -26,7 +26,8 @@ const Checkout = ({ cart, removeFromCart }) => {
     setTax(
       cart.reduce(
         (acc, curr) =>
-          acc + (Number(curr.price) * (1.0725).toFixed(2) * curr.qty) % (curr.qty),
+          acc +
+          ((Number(curr.price) * (1.0725).toFixed(2) * curr.qty) % curr.qty),
         0
       )
     );
@@ -64,91 +65,122 @@ const Checkout = ({ cart, removeFromCart }) => {
             p: "3%",
           }}
         >
-          {cart.map((cartItems) => (
-            <Card
-              key={cartItems.id}
-              component="div"
-              sx={{
-                width: "94%",
-                p: "3%",
-                position: "relative",
-                mt: "1%",
-                mb: ".5%",
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <Box
-                component="div"
-                sx={{
-                  width: 200,
-                  height: 100,
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  image={cartItems.image}
+          {cart.length > 0 ? (
+            <div>
+              {cart.map((cartItems) => (
+                <Card
+                  key={cartItems.id}
+                  component="div"
                   sx={{
-                    maxHeight: 100,
-                    maxwidth: "150px",
-                  }}
-                ></CardMedia>
-              </Box>
-              <Typography
-                sx={{
-                  fontWeight: "bold",
-                  fontFamily: "Playfair Display",
-                  fontStyle: "serif",
-                  fontSize: "15px",
-                  textOverflow: "ellipsis",
-                  width: "150px",
-                  overflow: "hidden",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                {cartItems.title}
-              </Typography>
-              <Box>
-                <Box
-                  sx={{
+                    width: "94%",
+                    p: "3%",
+                    position: "relative",
+                    mt: "1%",
+                    mb: ".5%",
+                    display: "flex",
                     justifyContent: "space-between",
                   }}
-                ></Box>
-              </Box>
-              <Box>
-                <Typography>x{cartItems.qty}</Typography>
-              </Box>
+                >
+                  <Box
+                    component="div"
+                    sx={{
+                      width: 200,
+                      height: 100,
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      image={cartItems.image}
+                      sx={{
+                        height: 100,
+                        maxWidth: 200,
+                      }}
+                    ></CardMedia>
+                  </Box>
+                  <Box
+                    sx={{
+                      pl: "2%",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        fontWeight: "bold",
+                        fontFamily: "Playfair Display",
+                        fontStyle: "serif",
+                        fontSize: "15px",
+                        textOverflow: "ellipsis",
+                        width: "150px",
+                        overflow: "hidden",
+                        whiteSpace: "nowrap",
+                      }}
+                    >
+                      {cartItems.title}
+                    </Typography>
+                    <Box>
+                      <Typography>x{cartItems.qty}</Typography>
+                    </Box>
+                  </Box>
+
+                  <Typography
+                    sx={{
+                      fontFamily: "Playfair Display",
+                      fontStyle: "serif",
+                      fontSize: "17px",
+                      mt: "-.5%",
+                    }}
+                  >
+                    ${cartItems.price * cartItems.qty}
+                  </Typography>
+                  <Button
+                    sx={{
+                      color: "rgb(11, 14, 48)",
+                      background: "white",
+                      height: "fit-content",
+                      pt: "0px",
+                      "&:hover": {
+                        color: "red",
+                        background: "white",
+                      },
+                    }}
+                    onClick={() => {
+                      removeFromCart(cartItems.id);
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      size="lg"
+                      icon={faTrashAlt}
+                    ></FontAwesomeIcon>
+                  </Button>
+                </Card>
+              ))}
+            </div>
+          ) : (
+            <div>
               <Typography
+                variant="h5"
+                component="div"
+                gutterBottom
                 sx={{
                   fontFamily: "Playfair Display",
                   fontStyle: "serif",
-                  fontSize: "17px",
-                  mt: "-.5%",
+                  textAlign: "center",
                 }}
               >
-                ${cartItems.price * cartItems.qty}
+                Your Cart is Empty!
               </Typography>
-              <Button
+              <Typography
+                variant="h7"
+                component="div"
                 sx={{
-                  color: "rgb(11, 14, 48)",
-                  background: "white",
-                  height: "fit-content",
-                  pt: "0px",
-                  "&:hover": {
-                    color: "red",
-                    background: "white",
-                  },
-                }}
-                onClick={() => {
-                  removeFromCart(cartItems.id);
+                  fontFamily: "Playfair Display",
+                  fontStyle: "serif",
+                  textAlign: "center",
                 }}
               >
-                <FontAwesomeIcon size="lg" icon={faTrashAlt}></FontAwesomeIcon>
-              </Button>
-            </Card>
-          ))}
+                Go back to order.
+              </Typography>
+            </div>
+          )}
         </Box>
         <Box
           sx={{
@@ -163,9 +195,33 @@ const Checkout = ({ cart, removeFromCart }) => {
               textAlign: "center",
             }}
           >
-            <Typography>Total: ${total.toFixed(2)}</Typography>
-            <Typography>Taxes: ${tax.toFixed(2)}</Typography>
-            <Typography>Subtotal: ${subtotal.toFixed(2)}</Typography>
+            <Typography
+              sx={{
+                fontFamily: "Playfair Display",
+                fontStyle: "serif",
+                fontSize: "17px",
+              }}
+            >
+              Total: ${total.toFixed(2)}
+            </Typography>
+            <Typography
+              sx={{
+                fontFamily: "Playfair Display",
+                fontStyle: "serif",
+                fontSize: "17px",
+              }}
+            >
+              Taxes: ${tax.toFixed(2)}
+            </Typography>
+            <Typography
+              sx={{
+                fontFamily: "Playfair Display",
+                fontStyle: "serif",
+                fontSize: "17px",
+              }}
+            >
+              Subtotal: ${subtotal.toFixed(2)}
+            </Typography>
           </Box>
           <Box
             sx={{
